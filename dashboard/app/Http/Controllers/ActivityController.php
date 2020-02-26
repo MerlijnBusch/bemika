1bc25c8a31d69bed22fb0bd337e40804fb6fe2ad
Merlijn Busch <24628@ma-web.nl>
2020-02-26 12:34:17 +0100

    added models

---
+++ dashboard/app/Http/Controllers/ActivityController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\activity;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ActivityController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return void
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param activity $activity
+     * @return void
+     */
+    public function show(activity $activity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param activity $activity
+     * @return void
+     */
+    public function edit(activity $activity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param activity $activity
+     * @return void
+     */
+    public function update(Request $request, activity $activity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param activity $activity
+     * @return void
+     */
+    public function destroy(activity $activity)
+    {
+        //
+    }
+}
