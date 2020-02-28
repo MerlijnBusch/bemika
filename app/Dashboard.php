7dd102df1907357f632596e53aab371e76cbaad1
Merlijn Busch <24628@ma-web.nl>
2020-02-28 15:18:02 +0100

    added filters

--- app/Dashboard.php
+++ app/Dashboard.php
@@ -5,6 +5,7 @@ namespace App;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 use stdClass;
 
 class Dashboard extends Model
@@ -27,17 +28,51 @@ class Dashboard extends Model
             ->get();
     }
 
-    public function fillArrayWithPlannedActivities($monthArray, $plannedActivities){
+    /**
+     * @param $array
+     * @param $plannedActivities
+     * @param int $weekInt
+     * @return false|string
+     * @throws Exception
+     */
+    public function fillArrayWithPlannedActivities($array, $plannedActivities, $weekInt = 0){
         foreach ($plannedActivities as $plannedActivity){
-            $day = substr(explode("-", new Carbon($plannedActivity->planned_date))[2], 0, 2);
+            $day = (substr(explode("-", new Carbon($plannedActivity->planned_date))[2], 0, 2) - $weekInt);
             $tmpObject = new stdClass();
             $tmpObject->planned_date = $plannedActivity->planned_date;
             $tmpObject->activity = Activity::find($plannedActivity->activity_id);
             $tmpObject->patient = Patient::find($plannedActivity->patient_id);
-            array_push($monthArray[$day], $tmpObject);
+            array_push($array[$day], $tmpObject);
         }
 
-        return json_encode($monthArray, true);
+        return json_encode($array, true);
+    }
+
+    /**
+     * @param $date
+     * @return false|string
+     * @throws Exception
+     */
+    public function generateMonthArray(Carbon $date){
+        $startOfMonth = $date->copy()->startOfMonth();
+        $endOfMonth = $date->copy()->endOfMonth();
+        $totalDays = $this->days_in_month($date->month, $date->year);
+        $monthArray = $this->generateArrayOfAllDaysInMonth($totalDays);
+
+        $plannedActivities = $this->getAllPlannedActivitiesInTimePeriodForUser(Auth::id(), $startOfMonth, $endOfMonth);
+        return $this->fillArrayWithPlannedActivities($monthArray, $plannedActivities);
+    }
+
+    /**
+     * @param $date
+     * @return Carbon
+     */
+    public function parseCarbonDate($date){
+        try {
+            return Carbon::parse(date_create($date));
+        } catch (Exception $e) {
+            abort('500');
+        }
     }
 
     /**
@@ -72,20 +107,5 @@ class Dashboard extends Model
      */
     public function days_in_month($month, $year){
         return $month == 2 ? ($year % 4 ? 28 : ($year % 100 ? 29 : ($year % 400 ? 28 : 29))) : (($month - 1) % 7 % 2 ? 30 : 31);
-    }
-
-    /**
-     * @param $date
-     * @return stdClass
-     * @throws Exception
-     */
-    public function getWeekStartAndEndDate($date) {
-        $date = new Carbon($date);
-
-        $week = new stdClass();
-        $week->start = $date->startOfWeek();
-        $week->end = $week->start->copy()->endOfWeek();
-
-        return $week;
     }
 }
--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -29,39 +29,47 @@ class DashboardController extends Controller
     }
 
     public function dashboard(){
-
         $dashboard = new Dashboard;
-        $totalDays = $dashboard->days_in_month(Carbon::now()->month, Carbon::now()->year);
-        $monthArray = $dashboard->generateArrayOfAllDaysInMonth($totalDays);
-
-        $plannedActivities = $dashboard->getAllPlannedActivitiesInTimePeriodForUser(Auth::id(), Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth());
-        $monthArray = $dashboard->fillArrayWithPlannedActivities($monthArray, $plannedActivities);
-
+        $monthArray = $dashboard->generateMonthArray(Carbon::now());
         return view('dashboard', ['data' => $monthArray]);
-
     }
 
     public function month($date){
-        $date = $this->makeNewDateObject($date);
-        dd($date);
+        $dashboard = new Dashboard;
+        $date = $dashboard->parseCarbonDate($date);
+        return response()->json($dashboard->generateMonthArray($date));
     }
 
-    public function week($date){
-        $date = $this->makeNewDateObject($date);
-        dd($date);
+    public function week($date){// note future self this per week so that means that 24 / 30 is same week and will put on same place doesnt matter if ur between day 24 - 30
+        $dashboard = new Dashboard;
+        $weekArray = $dashboard->generateArrayOfAllDaysInWeek();
+        $date = $dashboard->parseCarbonDate($date);
+        $startOfWeek = $date->copy()->startOfWeek();
+        $endOfWeek = $date->copy()->endOfWeek();
+
+        $weekArray = $dashboard->fillArrayWithPlannedActivities($weekArray, $dashboard->getAllPlannedActivitiesInTimePeriodForUser(Auth::id(), $startOfWeek, $endOfWeek), $startOfWeek->day);
+        return response()->json($weekArray);
     }
 
     public function day($date){
-        $date = $this->makeNewDateObject($date);
-        dd($date);
-    }
+        $dashboard = new Dashboard;
+        $date = $dashboard->parseCarbonDate($date);
 
-    private function makeNewDateObject($date){
-        try {
-            return new Carbon($date);
-        } catch (\Exception $e) {
-            abort('500', 'invalid date object');
+        $startOfDay = $date->copy()->startOfDay();
+        $endOfDay = $date->copy()->endOfDay();
+
+        $plannedActivities = $dashboard->getAllPlannedActivitiesInTimePeriodForUser(Auth::id(), $startOfDay, $endOfDay);
+
+        $activities = array();
+        foreach ($plannedActivities as $plannedActivity){
+            $tmpObject = new stdClass();
+            $tmpObject->planned_date = $plannedActivity->planned_date;
+            $tmpObject->activity = Activity::find($plannedActivity->activity_id);
+            $tmpObject->patient = Patient::find($plannedActivity->patient_id);
+            array_push($activities, $tmpObject);
         }
+
+        return response()->json(json_encode($activities, true));
     }
 
 }
