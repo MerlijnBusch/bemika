8efe9742d247c799fb4b13489c890b12458286bb
Merlijn Busch <24628@ma-web.nl>
2020-02-28 12:17:12 +0100

    changed map structure

--- public/js/app.js
+++ public/js/app.js
@@ -49697,8 +49697,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/merlijn/PhpstormProjects/bemika/dashboard/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/merlijn/PhpstormProjects/bemika/dashboard/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/merlijn/PhpstormProjects/dashboard-robin-assistant/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/merlijn/PhpstormProjects/dashboard-robin-assistant/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
