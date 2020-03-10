bfc67b1a9c0c67be2937e1f67473e42121f5586f
ticho10 <poffertjeskip@gmail.com>
2020-03-10 09:17:31 +0100

    Updated navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class="layouts-nav">
     <a href="{{route('dashboard')}}" class="layouts-nav-app-name">
-        {{env('APP_NAME')}}
+        <!-- {{env('APP_NAME')}} -->
+        <img src="{{url('/logo-images/robinlogo.png')}}" alt="Logo Robin Assistant" class="layouts-nav-image-logo">
     </a>
-
-    <div class="layouts-nav-icon-container">
+    <div class="layouts-nav-set-language">
         <form method="post" action="{{ route('user.setLanguage') }}">
             @method('POST')
             @csrf
@@ -11,12 +11,14 @@
             {{ Form::select('lang', Config::get('languages'), old('lang', $user->lang ?? ''), ['class' => '']) }}
             {{ Form::submit('Save', ['name' => 'submit'], ['class' => '']) }}
         </form>
+    </div>
+    <div class="layouts-nav-icon-container">
         <a href="{{route('user.profile')}}" class="layouts-nav-user">
-            {{trans('labels.welcome')}}, {{Auth::user()->name}}
-            <i class="material-icons">person</i>
+            <span class="layouts-nav-icon-name">{{trans('labels.welcome')}}, {{Auth::user()->name}}</span>
+            <i class="material-icons layouts-nav-person-icon">person</i>
         </a>
-        <a href="#"><i class="material-icons layouts-nav-icon">camera_roll</i></a>
-        <a href="#"><i class="layouts-nav-icon">?</i></a>
+        <a href="#"><i class="material-icons layouts-nav-icon">videocam</i></a>
+        <a href="#"><i class="layouts-nav-icon layouts-nav-icon-question">?</i></a>
         <a class="dropdown-item" href="{{ route('logout') }}" title="{{trans('labels.logout')}}"
            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
             <i class="material-icons layouts-nav-icon">subdirectory_arrow_right</i>
