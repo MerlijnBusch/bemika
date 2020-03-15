c43c12cfac6e5b375ad5e14889260eef4d1e9a6e
Tim <34155548+TotallyTheTim@users.noreply.github.com>
2020-03-15 21:24:22 +0100

    trying to get the callender to work and not break development at the same time

---
+++ resources/js/components/CalendarTaskComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="tasks">
+        <div v-for="dot in dots" :key="dot.patient" :style="{backgroundColor : dot.patient.color_code}">
+            {{dot.activities.length}}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['tasks'],
+        data() {
+            return {
+                dots: [],
+            }
+        },
+        mounted() {
+            console.log(this.tasks);
+            for (let i = 0; i < this.tasks.length; i++) {
+                // console.log(this.dots.findIndex(d => d.patient === this.tasks[i].patient));
+                if (this.dots.findIndex(d => d.patient.id === this.tasks[i].patient.id) === -1) {
+                    this.dots.push(
+                        {patient: this.tasks[i].patient,
+                            activities: [this.tasks[i].activity],
+                        });
+                } else {
+                    this.dots[this.dots.findIndex(d => d.patient.id === this.tasks[i].patient.id)].activities.push(this.tasks[i].activity);
+                }
+            }
+        }
+    }
+</script>
